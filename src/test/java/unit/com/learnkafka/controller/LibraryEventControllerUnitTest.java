242d337484e67e4a69a3b263af1cef457f61edc8
Dileep Naik <dileep.naik1@gmail.com>
2020-08-03 22:04:20 +0530

    Unit Testing completed

--- src/test/java/unit/com/learnkafka/controller/LibraryEventControllerUnitTest.java
+++ src/test/java/unit/com/learnkafka/controller/LibraryEventControllerUnitTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -44,7 +45,7 @@ public class LibraryEventControllerUnitTest {
                 .build();
 
         String json = objectMapper.writeValueAsString(libraryEvent);
-        doNothing().when(libraryEventProducer).sendLibraryApproach2(isA(LibraryEvent.class));
+        when(libraryEventProducer.sendLibraryApproach2(isA(LibraryEvent.class))).thenReturn(null);
 
         mockMvc.perform(post("/v1/libraryevent")
                 .content(json)
@@ -68,9 +69,10 @@ public class LibraryEventControllerUnitTest {
                 .build();
 
         String json = objectMapper.writeValueAsString(libraryEvent);
-        doNothing().when(libraryEventProducer).sendLibraryApproach2(isA(LibraryEvent.class));
 
-        String expectedErrorMessage =  "book.bookAuthor - must not be blank, book.bookId - must not be null";
+        when(libraryEventProducer.sendLibraryApproach2(isA(LibraryEvent.class))).thenReturn(null);
+
+        String expectedErrorMessage = "book.bookAuthor - must not be blank, book.bookId - must not be null";
 
         mockMvc.perform(post("/v1/libraryevent")
                 .content(json)
